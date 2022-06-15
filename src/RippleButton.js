63f3ef57f39049d7186cae9becccda6dbbf0c060
Pete Chu <pchu@forbes.com>
2022-06-15 10:35:17 -0400

    ripple button

---
+++ src/RippleButton.js
@@ -0,0 +1,33 @@
+import "./RippleButton.css";
+
+const addListener = () => {
+  const rippleButton = document.querySelector('.ripple-button');
+  
+  function mousePositionToCustomProp(event, element) {
+    let posX = event.offsetX;
+    let posY = event.offsetY;
+
+    element.style.setProperty('--x', posX + 'px');
+    element.style.setProperty('--y', posY + 'px');
+
+  }
+
+  rippleButton.addEventListener('click', (e) => {
+    mousePositionToCustomProp(e, rippleButton);
+    rippleButton.classList.add('pulse');
+    rippleButton.addEventListener('animationend', () => {
+      rippleButton.classList.remove('pulse');
+    })
+  });
+  
+}
+
+export default function RippleButton() {
+  return (
+    <div>
+      <button className="ripple-button">Click Me</button>
+    </div>
+  );
+}
+
+setTimeout(addListener);

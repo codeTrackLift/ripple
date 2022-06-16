d2a8eeac1157e5b5ab51e675e3126b73ab33f512
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-06-15 20:23:44 -0400

    ButtonControls

---
+++ src/ButtonControls.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+
+const buttonStyle = {
+  backgroundColor: 'rgb(50,50,50)',
+  color: 'white',
+  padding: '0.5rem 1rem',
+  border: '1px solid white',
+  boxShadow: 'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px',
+}
+
+const inputStyle = {
+  boxShadow: 'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px',
+}
+
+export const ButtonControls = ({lightMode, setLightMode}) => {
+  const [color, setColor] = useState('#303030');
+
+  const onChange = (e) => {
+    setColor(e.target.value);
+  }
+
+  useEffect(() => {
+    document.body.style.backgroundColor = color;
+  }, [color])
+
+  return (
+    <>
+      <h1 style={{margin:'0 auto'}}>click button to make a ripple</h1>
+      <div>
+        <label htmlFor="colorpicker">background color: </label>
+        <input 
+          type="color" 
+          id="colorpicker" 
+          value={color}
+          onChange={onChange}
+          style={inputStyle}
+        ></input>
+      </div>
+      <div>
+        <span htmlFor='buttonColor'>button variant: </span>
+        <button 
+          onClick={() => setLightMode(!lightMode)}
+          style={buttonStyle}
+        >{lightMode ? 'Light Mode' : 'Dark Mode'}</button>
+      </div>
+    </>
+  )
+}
--- src/RippleButton.jsx
+++ src/RippleButton.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { ButtonControls } from './ButtonControls'
 import "./RippleButton.css";
 
 const addListener = () => {
@@ -20,43 +21,18 @@ const addListener = () => {
       rippleButton.classList.remove('pulse');
     })
   });
-  
 }
 
 export default function RippleButton() {
   const [lightMode, setLightMode] = useState(false);
-  const [color, setColor] = useState('#505050');
-
-  const onChange = (e) => {
-    setColor(e.target.value);
-  }
-
-  useEffect(() => {
-    document.body.style.backgroundColor = color;
-  }, [color])
 
   return (
     <div id='container'>
-      <h1 style={{margin:'0 auto'}}>click button to make a ripple</h1>
-      <div>
-        <label htmlFor="colorpicker">background color: </label>
-        <input 
-          type="color" 
-          id="colorpicker" 
-          value={color}
-          onChange={onChange}
-        ></input>
-      </div>
-      <div>
-        <span htmlFor='buttonColor'>button color: </span>
-        <button 
-          onClick={() => setLightMode(!lightMode)}
-        >{lightMode ? 'Light Mode' : 'Dark Mode'}</button>
-      </div>
+      <ButtonControls lightMode={lightMode} setLightMode={setLightMode} />
 
-      <button 
-        className={lightMode ? 'ripple-button light' : 'ripple-button'}
-      >Click Me</button>
+      <button className={lightMode ? 'ripple-button light' : 'ripple-button'}>
+        Click Me
+      </button>
     </div>
   );
 }
